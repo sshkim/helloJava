81c81a4947547133c948e11c8200bd216432c681
SeungHwan Kim <Super@SeungHwanui-MacBook-Pro.local>
2016-10-20 15:20:14 +0900

    IntToString

--- src/main/com/sshkim/algorithm/IntToString.java
+++ src/main/com/sshkim/algorithm/IntToString.java
@@ -1,27 +1,57 @@
 package main.com.sshkim.algorithm;
 
+import org.junit.Assert;
+
 /**
  * Created by sshkim on 2016. 10. 19..
  */
 public class IntToString {
 
+    /**
+     * Explain is below...
+     *
+     *
+     * A = 0, B = 1, C = 2 ...     Z = 25                                   Total : 26
+     * AA = 26, AB = 27 ...        ZZ = 26 * 26 + 25                        Total : 26 * 26
+     * AAA = 702, AAB = 703 ...    ZZZ = (26 * 26 * 26) + (26 * 26 + 25)    Total : 26 * 26 * 26
+     * .
+     * .
+     * .
+     * .
+     */
+
+    private static final int totalAlpabets = 26;
+
+
     public static void main(String[] args) {
-        int num = 55;
 
 
-        /**
-         * A = 0, B = 1, C = 2 ...     Z = 25
-         * AA = 26, AB = 27 ...        ZZ = 26 * 26 + 25
-         * AAA = 702, AAB = 703 ...    ZZZ = (26 * 26 * 26) + (26 * 26 + 25)
-         * .
-         * .
-         * .
-         * .
-         */
+        int num = 25;
 
+        String result = toAlphabet(num);
 
+        Assert.assertEquals("Z", result);
 
+    }
 
+    private static String toAlphabet(int num) {
+        StringBuilder sb = new StringBuilder();
+        int realNumber = num + 1;
+        int remainder;
+        while (realNumber > 0) {
+            remainder = realNumber % totalAlpabets;
+
+            if (remainder == 0) {
+                sb.insert(0, "Z");
+                realNumber -= totalAlpabets;
+            } else {
+                sb.insert(0, (char) ('A' + (remainder - 1)));
+            }
+
+            realNumber /= totalAlpabets;
+
+        }
+        return sb.toString();
     }
 
 }
